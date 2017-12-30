82edb6fe34e60bfed1b6531c64d27b01ff2288b9
Unknown <geoffreydu38@hotmail.fr>
2017-12-30 04:17:37 +0100

    2e scene : img,clavier,reponse

---
+++ Assets/Clavier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Clavier : MonoBehaviour {
+
+    // Use this for initialization
+    public float offSetX = 40;
+    
+    public Button btn_clavier;
+	void Start () {
+        offSetX += btn_clavier.image.rectTransform.sizeDelta.x / 2f; //Pour prendre en compte la taille du bouton
+        float totalWidth = Screen.width - offSetX * 2;//offSetX*2 pour prend que ce soit offset à gauche et à droite
+        float startPos = (totalWidth) /-2;
+        float ecart = (totalWidth / 6);
+        for (int k = 0; k < 2; k++)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Button b = Instantiate<Button>(btn_clavier);
+                b.transform.SetParent(transform);
+                b.transform.localPosition = new Vector3(startPos + i*ecart, k*40, 0);
+            }
+        }
+	}
+	
+}
---
+++ Assets/Reponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Button))]
+public class Reponse : MonoBehaviour {
+
+    public Button btn;
+    public float spacing = 2f;
+    public float totalSize = 0f;
+    private float w_btn; //width button
+    private string nom = Scenes.Nom;
+    private List<Button> lb = new List<Button>();
+	void Start () {
+        w_btn = btn.image.rectTransform.sizeDelta.x;
+        spacing += w_btn;
+        for (int i=0;i<nom.Length;i++)
+        {
+            if (!nom[i].Equals(" "))
+            { 
+                Button b = Instantiate<Button>(btn);
+                b.GetComponentInChildren<TextMeshProUGUI>().text = nom[i].ToString();
+                b.transform.SetParent(transform);
+                b.transform.localPosition = new Vector3(i * spacing, 0, 0);
+                lb.Add(b);
+            }
+            totalSize += spacing;
+        }
+        SetPos();
+	}
+	
+	// Update is called once per frame
+	private void SetPos()
+    {
+        float startPos = (Screen.width - totalSize)/-2;
+        int length = lb.Count;
+        for (int i=0;i<length;i++)
+        {
+                lb[i].transform.position += new Vector3(startPos,0,0);
+        }
+    }
+}
